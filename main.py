dae664c460cdcaec69366d74f1e37205636f8de4
Christian Urcuqui <ulcamilo@gmail.com>
2024-01-31 22:50:35 -0500

    NEW

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyaudio
 import wave
+from transformers import pipeline, AutoModelForCausalLM, AutoModelForSpeechSeq2Seq, AutoProcessor
+import torch
+from audio2numpy import open_audio
 
 def recording():
     chunk = 1024  # Record in chunks of 1024 samples
@@ -41,4 +44,43 @@ def recording():
     wf.writeframes(b''.join(frames))
     wf.close()
 
-recording()
+def get_text():
+        
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    
+    assistant_model_id = "distil-whisper/distil-large-v2"
+    
+    assistant_model = AutoModelForCausalLM.from_pretrained(
+        assistant_model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    assistant_model.to(device)
+    
+    model_id = "openai/whisper-large-v2"
+    
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
+    
+    processor = AutoProcessor.from_pretrained(model_id)
+    
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        generate_kwargs={"assistant_model": assistant_model},
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+   
+    path = "output.wav"
+    signal, sampling_rate = open_audio(path)
+    prediction = pipe(signal, batch_size=8)["text"]
+    print(prediction)
+
+#recording()
+get_text()
+
